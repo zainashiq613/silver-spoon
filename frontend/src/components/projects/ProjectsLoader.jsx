d9bed03089b0adb52f121f001f153371d799fe58
Muhammad Zain <zain.ashiq613@gmail.com>
2025-08-17 20:35:54 +0500

    projects detail page and changes

---
+++ frontend/src/components/projects/ProjectsLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectsLoader() {
+  return (
+    <div className="flex justify-center items-center pt-10">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-[#5FCDB2]"></div>
+    </div>
+  );
+}
+
+export default ProjectsLoader;
---
+++ frontend/src/pages/projects/ProjectDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProjectDetails() {
+  return <div className="pt-35 pb-20">ProjectDetails</div>;
+}
+
+export default ProjectDetails;
